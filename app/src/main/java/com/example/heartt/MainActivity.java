3f8528e880c0438ea9efb07d4b95a4107bc09d24
Jordan <jnlove314@gmail.com>
2020-02-08 17:50:05 -0600

    splash page boilerplate

--- app/src/main/java/com/example/heartt/MainActivity.java
+++ app/src/main/java/com/example/heartt/MainActivity.java
@@ -4,21 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIMEOUT = 2500;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
     }
 
-    public void sendMessage(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
-
-    }
+//    public void sendMessage(View view) {
+//        // Do something in response to button
+//        Intent intent = new Intent(this, HomeActivity.class);
+//        startActivity(intent);
+//
+//    }
 }
