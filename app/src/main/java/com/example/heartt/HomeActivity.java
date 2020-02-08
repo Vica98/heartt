450d3255ec6d1f7d79edbe7a9d621cba02b304fa
Victoria <vica.chevchenco@gmail.com>
2020-02-08 15:42:47 -0600

    Implemented prototype for Home page and Menu

--- app/src/main/java/com/example/heartt/HomeActivity.java
+++ app/src/main/java/com/example/heartt/HomeActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
@@ -15,9 +17,57 @@ public class HomeActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
     }
-    public void sendMessage(View view) {
+    public void sendMessage1(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, SponsorInfo.class);
         startActivity(intent);
     }
+
+    public void sendMessage2(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, SponsorInfo.class);
+        startActivity(intent);
+    }
+
+    public void sendMessage3(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, SponsorInfo.class);
+        startActivity(intent);
+    }
+
+    public void sendMessage4(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, SponsorInfo.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the main_menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.item1:
+                Intent intent1 = new Intent(this, HomeActivity.class);
+                startActivity(intent1);
+                break;
+            case R.id.item2:
+                Intent intent2 = new Intent(this, HistoryActivity.class);
+                startActivity(intent2);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
+    }
+
+
 }
+
+
