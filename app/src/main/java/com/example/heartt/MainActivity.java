e4e4c97fb657a6c3b1519351ac98f48637ebc591
AnjaliPare <anjali.pare@gmail.com>
2020-02-08 14:23:52 -0600

    Sandwich sponsor page

--- app/src/main/java/com/example/heartt/MainActivity.java
+++ app/src/main/java/com/example/heartt/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.heartt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sponsor(View view) {
+        Intent intent = new Intent(this, SponsorInfo.class);
+        startActivity(intent);
+        // Do something in response to button
     }
 }
