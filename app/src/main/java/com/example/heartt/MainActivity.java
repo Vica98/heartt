0177474c30ed8b0150c824f5fdd23b85ce46ab68
Jordan <jnlove314@gmail.com>
2020-02-08 18:58:11 -0600

    splash page without logo

--- app/src/main/java/com/example/heartt/MainActivity.java
+++ app/src/main/java/com/example/heartt/MainActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-
+import android.view.WindowManager;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         new Handler().postDelayed(new Runnable(){
             @Override
