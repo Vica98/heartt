0a7a3a37f6f4d6804755980df8deceb7d87a5a34
Victoria <vica.chevchenco@gmail.com>
2020-02-09 04:24:41 -0600

    Added more features to info page

--- app/src/main/java/com/example/heartt/InfoPageActivity.java
+++ app/src/main/java/com/example/heartt/InfoPageActivity.java
@@ -2,6 +2,7 @@ package com.example.heartt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class InfoPageActivity extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class InfoPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_page);
+
+        Intent intent = getIntent();
     }
 }
